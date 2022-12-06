b0fa8bcdbe19bf778f5dbd884e8512db1bd2957d
Fernando Gutiérrez <ofernandogh15@gmail.com>
2022-12-05 22:11:14 -0500

    Agregar archivos de proyecto.

---
+++ Abiru/Pages/Soporte.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Abiru.Pages
+{
+    public class SoporteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
